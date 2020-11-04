f41131615ded6a310047ee02131729fbd71d0532
martinmarinov150402 <martinmarinov150402@gmail.com>
2020-11-04 22:05:05 +0200

    Init typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host:'localhost',
+      username: 'taxiZilla',
+      password: 'VhCNrHnMEB3sE?9_',
+      database: 'taxiZilla',
+      entities:[],
+      synchronize:true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
