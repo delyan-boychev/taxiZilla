c496449ea6ece497c2f4df74cb89f2672755d019
Martin Marinov <martinmarinov150402@gmail.com>
2021-01-21 23:08:08 +0200

    Remove something useless

--- src/response.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const Resp = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-      const request = ctx.switchToHttp().getResponse();
-      return request;
-    },
-  );
